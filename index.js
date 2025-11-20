87fe010372e8c9308286009cd64ebee041fdac3b
Kuba István Alexander <votabuk@gmail.com>
2025-11-20 10:45:35 +0100

    Initial commit: Node.js + Express hello world app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Helló DevOps! Üdv a Node.js + Express alkalmazásból!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
